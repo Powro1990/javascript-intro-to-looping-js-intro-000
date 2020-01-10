fed25789fec0fa1ae64385aab6374bf6e57136bf
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2020-01-10 17:58:36 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,10 +3,20 @@ function forLoop(array){
     if(i===1){
       array.push('I am 1 strange loop.');
     }else{
-      //array.push('I am ${i} strange loops.');
-      array.push("I am ${i} strange loops.");
+      array.push('I am ${i} strange loops.');
     }
     
+  }
+  return array;
+}
+
+function forLoop(array) {
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
   }
   return array;
 }
