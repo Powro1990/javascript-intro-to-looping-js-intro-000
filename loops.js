f983819589f8a1576ed36682c09d5bf155b10bd9
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2020-01-10 18:24:46 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,6 +17,11 @@ function whileLoop(n){
   return('done');
 }
 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
 function doWhileLoop(num){
   do{
     console.log("I run once regardless.")
